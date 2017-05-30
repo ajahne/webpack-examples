6b1511b23c65aa2d370f35f8d8d8c33f63f21659
ajahne <ajahne@gmail.com>
2017-05-29 21:27:26 -0400

    initial commit of files based on webpack intro tutorial

---
+++ hello-world/app/index.js
@@ -0,0 +1,10 @@
+function component() {
+  var element = document.createElement('div');
+
+  //loadash is required for this next line to work
+  element.innerHTML = _.join(['Hello','webpack'], '');
+
+  return element;
+}
+
+document.body.appendChild(component());
